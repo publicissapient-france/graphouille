e730cc3aa989395f78e44c2981b43404f871a8aa
Aurore de Amaral <aurore@deamaral.fr>
2020-11-09 11:11:42 +0100

    Add json to build

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    extraFileExtensions: "json",
     project: "tsconfig.json",
     sourceType: "module",
   },
